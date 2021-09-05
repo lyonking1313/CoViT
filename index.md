81f71faf29b6fb809af608805e853811a87a1352
lyonking1313 <lyonkim2004@icloud.com>
2021-09-05 14:31:57 -0400

    test

--- index.md
+++ index.md
@@ -6,6 +6,8 @@ feature_image: "/images/covid.jpg"
 
 ## CoViT (Covid Variant Tracker)
 
+Keke is a guy
+
 This site tracks multiple different variants of COVID-19 in the U.S. Our goal is to make this information publicly and easily accessible to study and understand the trends of COVID-19. Thanks to recent efforts of public labs, there has been an increase in sequencing data for this disease. This data is primarily stored in [GISAID](https://www.gisaid.org) (Global Initiative on Sharing All Influenza Data). However, it is not easily accessible to the public and is difficult to understand. We use this data in tandem with our own statistical analysis to provide visual representations and graphs. More specifically, we present weekly updates to quantify the prevalence of COVID-19 variants over time. In the future, we will create a model to predict the future trajectory of these variants. 
 
 We will measure the number of cases at each epiweek. The epiweek system is a standardized numbering of the weeks of 2021. For example, epiweek 12 are the days from 3/21/21-3/27/21.  Data from the most recent week may not be complete. Click here for the full [epiweek calendar](https://ibis.health.state.nm.us/resource/MMWRWeekCalendar.html).  
--- python/CoViT_script.py
+++ python/CoViT_script.py
@@ -32,13 +32,43 @@ def all_functions(variant_name, denom):
 
 		transmissibility_graph(midpoint_x, incubation_m_list, variant_name)
 
+
+midpoint_x_list = {}
+incubation_m_list_list = {}
+
+def all_transmissibility_ratios()
+
+
+	for i in range (0, len(top_cases_by_variant)):
+
+		k_list, n_list, file_name, start_index, B117_over_total_USA, B117_per_week, USA_per_week = set_up_variables(variant_name, denom)
+
+		lower_bounds, upper_bounds, max_epiweek = get_upper_lower_bounds(k_list, n_list, file_name, start_index)
+			
+		lin_x, lin_y, x_list, y_list, y_logistic_scale, lin_reg_y = logit_function(max_epiweek, start_index, B117_over_total_USA, k_list)
+
+		logit_lower_bounds, logit_upper_bounds = logit_bounds(lower_bounds, upper_bounds)
+
+		midpoint_x, midpoint_list, incubation_m_list, log_reg_y, k_list_int = last_variables(lin_y, k_list, x_list, y_list)
+
+		top_cases_by_variant, total_cases = pie_chart_variables()
+
+
+
+
+		midpoint_x_list[top_cases_by_variant.keys()[i]] = midpoint_x
+		incubation_m_list_list[top_cases_by_variant.keys()[i]] = incubation_m_list
+
+		transmissibility_graph(midpoint_x, incubation_m_list, variant_name)
+
+
  	
 pie()
 
-# all_functions("B.1.1.7", False)
+all_functions("B.1.1.7", False)
 
-# all_functions("P.1", False)
-# all_functions("P.1", True)
+all_functions("P.1", False)
+all_functions("P.1", True)
 
-# all_functions("B.1.617.2", False)
-# all_functions("B.1.617.2", True)
+all_functions("B.1.617.2", False)
+all_functions("B.1.617.2", True)
